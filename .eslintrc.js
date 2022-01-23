509fbb408d2bb3eabda8cb29ab3d9a9254dd29b8
Amin AIMEUR <m.amin.aimeur@gmail.com>
2022-01-23 05:24:13 +0100

    eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
+/* eslint-disable */
 module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': false,
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
+        'prettier/prettier': false,
+        '@typescript-eslint/no-shadow': ['warn'],
         'no-shadow': 'off',
         'no-undef': 'off',
       },
